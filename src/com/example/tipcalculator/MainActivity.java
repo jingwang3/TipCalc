35c76abef17bb82cace2812dee86c1dcf1e24535
jing <jingwang3@gmail.com>
2013-11-08 15:58:35 -0500

    add in tax and total calculations

--- src/com/example/tipcalculator/MainActivity.java
+++ src/com/example/tipcalculator/MainActivity.java
@@ -1,25 +1,34 @@
 package com.example.tipcalculator;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-	private EditText tipTotal, peopleTotal, tipRate, billTotal;
+	private EditText totalPerPerson, peopleTotal, tipRate, billBeforeTax, totalTax, mealTotal;
+	private InputMethodManager imm;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		billTotal = (EditText) findViewById(R.id.editText1);
+		billBeforeTax = (EditText) findViewById(R.id.editText1);
 		peopleTotal = (EditText) findViewById(R.id.editText2);
 		tipRate = (EditText) findViewById(R.id.editText3);
-		tipTotal = (EditText) findViewById(R.id.editText4);
-		tipTotal.setKeyListener(null);
+		totalTax = (EditText) findViewById(R.id.editText6);
+		mealTotal = (EditText) findViewById(R.id.editText5);
+		totalPerPerson = (EditText) findViewById(R.id.editText4);
+		totalPerPerson.setKeyListener(null);
+		mealTotal.setKeyListener(null);
+		
+		imm = (InputMethodManager)getSystemService(
+			      Context.INPUT_METHOD_SERVICE);
 
 	}
 
@@ -32,9 +41,14 @@ public class MainActivity extends Activity {
 
 	/** Called when the user touches the button */
 	public void calculateTotal(View view) {
+		int perPersonRate = 0;
+		int finalTotalBill = 0;
 		// Do something in response to button click
-		if(billTotal.getText().length() <= 0){
-			billTotal.setText("0");
+		if(billBeforeTax.getText().length() <= 0){
+			billBeforeTax.setText("0.0");
+		}
+		if(totalTax.getText().length() <= 0){
+			totalTax.setText("0.0");
 		}
 		if(peopleTotal.getText().length() <= 0){
 			peopleTotal.setText("1");
@@ -43,15 +57,22 @@ public class MainActivity extends Activity {
 			tipRate.setText("15");
 		}
 		
-		
 		try {
-			tipTotal.setText("$ "
-					+ Float.parseFloat(billTotal.getText().toString())
-					* (1 + Float.parseFloat(tipRate.getText().toString()) / 100)
+			perPersonRate = Math.round((Float.parseFloat(billBeforeTax.getText().toString())
+					* (1 + Float.parseFloat(tipRate.getText().toString()) / 100) + Float.parseFloat(totalTax.getText().toString()))
 					/ Float.parseFloat(peopleTotal.getText().toString()));
+			
+			finalTotalBill = Math.round(perPersonRate * Float.parseFloat(peopleTotal.getText().toString()));
+			
+			
+			totalPerPerson.setText("$ " + perPersonRate);
+			mealTotal.setText("$ " + finalTotalBill);
 		} catch (Exception e) {
 
 		}
+		
+		imm.hideSoftInputFromWindow(totalPerPerson.getWindowToken(), 0);
+		
 	}
 
 }
